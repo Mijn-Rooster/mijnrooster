fa0f3ecb83e8b53db23ab932d8acf76f1f063e70
David Jongeneel <87093557+DVDdisk7@users.noreply.github.com>
2024-09-12 19:13:21 +0000

    Add svelte-routing package and update routing in frontend

--- frontend/src/renderer/src/App.svelte
+++ frontend/src/renderer/src/App.svelte
@@ -1,40 +1,12 @@
 <script>
-  import Versions from './components/Versions.svelte'
-  import electronLogo from './assets/electron.svg'
+  import { Router, Route } from "svelte-routing";
+  import Home from "./routes/Home.svelte";
+  import Test from "./routes/Test.svelte";
 
-  const ipcHandle = () => window.electron.ipcRenderer.send('ping')
-
-  const apiRequest = async () => {
-    try {
-      const response = await fetch('http://localhost:8000');
-      const data = await response.json();
-      console.log(data);
-      alert(data.message);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      alert('Error fetching data:', error);
-    }
-  }
+  export let url = "";
 </script>
 
-<img alt="logo" class="logo" src={electronLogo} />
-<div class="creator">Powered by electron-vite</div>
-<div class="text">
-  Build an Electron app with
-  <span class="svelte">Svelte</span>
-</div>
-<p class="tip">Please try pressing <code>F12</code> to open the devTool</p>
-<div class="actions">
-  <div class="action">
-    <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">Documentation</a>
-  </div>
-  <div class="action">
-    <!-- svelte-ignore a11y-click-events-have-key-events a11y-no-static-element-interactions a11y-missing-attribute-->
-    <a target="_blank" rel="noreferrer" on:click={ipcHandle}>Send IPC</a>
-  </div>
-  <div class="action">
-    <!-- svelte-ignore a11y-click-events-have-key-events a11y-no-static-element-interactions a11y-missing-attribute-->
-    <a target="_blank" rel="noreferrer" on:click={apiRequest}>API call to backend</a>
-  </div>
-</div>
-<Versions />
+<Router {url}>
+    <Route path="/" component="{Home}" />
+    <Route path="/test" component="{Test}" />
+</Router>
---
+++ frontend/src/renderer/src/routes/Test.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { Link } from 'svelte-routing'
+  </script>
+
+<h1>Test Page</h1>
+<p>It works!</p>
+<div class="actions">
+    <div class="action">
+      <!-- svelte-ignore a11y-click-events-have-key-events a11y-no-static-element-interactions a11y-missing-attribute-->
+      <Link to="/">Back to home!</Link>
+    </div>
+  </div>
