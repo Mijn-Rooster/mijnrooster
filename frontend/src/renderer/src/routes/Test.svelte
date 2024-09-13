82486febf3fe7a20e48d924f0803a9b8db845afd
David Jongeneel <87093557+DVDdisk7@users.noreply.github.com>
2024-09-13 17:13:28 +0200

    12 development add router and flowbite (#13)
    * Add svelte-routing package and update routing in frontend
    
    * Added Flowbite and Tailwind CSS

---
+++ frontend/src/renderer/src/routes/Test.svelte
@@ -0,0 +1,54 @@
+<script>
+import { Link } from "svelte-routing";
+import { Tabs, TabItem } from "flowbite-svelte";
+</script>
+
+<h1>Test Page</h1>
+<p>It works!</p>
+<div class="actions">
+  <div class="action">
+    <!-- svelte-ignore a11y-click-events-have-key-events a11y-no-static-element-interactions a11y-missing-attribute-->
+    <Link to="/">Back to home!</Link>
+  </div>
+</div>
+
+<br />
+
+<Tabs>
+  <TabItem open title="Profile">
+    <p class="text-sm text-gray-500 dark:text-gray-400">
+      <b>Profile:</b>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+      incididunt ut labore et dolore magna aliqua.
+    </p>
+  </TabItem>
+  <TabItem title="Settings">
+    <p class="text-sm text-gray-500 dark:text-gray-400">
+      <b>Settings:</b>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+      incididunt ut labore et dolore magna aliqua.
+    </p>
+  </TabItem>
+  <TabItem title="Users">
+    <p class="text-sm text-gray-500 dark:text-gray-400">
+      <b>Users:</b>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+      incididunt ut labore et dolore magna aliqua.
+    </p>
+  </TabItem>
+  <TabItem title="Dashboard">
+    <p class="text-sm text-gray-500 dark:text-gray-400">
+      <b>Dashboard:</b>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+      incididunt ut labore et dolore magna aliqua.
+    </p>
+  </TabItem>
+  <TabItem disabled>
+    <span slot="title" class="text-gray-400 dark:text-gray-500">Disabled</span>
+    <p class="text-sm text-gray-500 dark:text-gray-400">
+      <b>Disabled:</b>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+      incididunt ut labore et dolore magna aliqua.
+    </p>
+  </TabItem>
+</Tabs>
