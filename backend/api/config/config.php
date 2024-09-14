9cc1c83cfc6906664f49a4f08850efd0ce874662
David Jongeneel <david.jongeneel@outlook.com>
2024-09-14 17:56:44 +0200

    Minor changes

--- backend/api/config/config.php
+++ backend/api/config/config.php
@@ -5,6 +5,6 @@ define('TENANT_NAME', 'Testschool');
 
 // Zermelo API
 define('PORTAL_URL', 'partner-7206.zportal.nl');
-define('ZERMELO_API_TOKEN', '')
+define('ZERMELO_API_TOKEN', '3d461m19fl3q36oviq4oc1m48b')
 
 ?>
