e923e0e244b19a3d18d33d2f6a24c17f9ec4b22b
David Jongeneel <87093557+DVDdisk7@users.noreply.github.com>
2024-10-08 17:05:48 +0000

    Include nav buttons

--- frontend/src/renderer/src/routes/Home.svelte
+++ frontend/src/renderer/src/routes/Home.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
   import MenuBar from "../components/MenuBar.svelte";
+  import { navigate } from "../stores/RouterStore";
 </script>
 
 <h1>Home</h1>
 
 <MenuBar />
+
+<!-- svelte-ignore a11y-click-events-have-key-events a11y-no-static-element-interactions a11y-missing-attribute-->
+<button on:click={() => navigate("/schedule")}
+  >[] Go to schedule page</button>
+<button on:click={() => navigate("/error")}
+  >[] Go to error page</button>
+<button on:click={() => navigate("/setup")}
+    >[] Go to setup page</button>
