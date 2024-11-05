aeb2b2d8ea0c57a5f13da6ab93b1a20e518ec1be
David Jongeneel <87093557+DVDdisk7@users.noreply.github.com>
2024-11-05 16:01:59 +0000

    Replaced student and teacher endpoint with general user endpoint and some minor fixes

---
+++ backend/src/Controllers/UserDetailsController.php
@@ -0,0 +1,118 @@
+<?php
+
+/**
+ * Zermelo user Controller
+ * 
+ * This file contains the logic for the userdetails endpoint.
+ */
+
+// Require the necessary files and classes
+require_once __DIR__ . '/../Services/Auth.php';
+require_once __DIR__ . '/../Services/ZermeloAPI.php';
+require_once __DIR__ . '/../Services/ErrorHandler.php';
+require_once __DIR__ . '/../Models/ResponseModel.php';
+require_once __DIR__ . '/../Models/ZermeloUserModel.php';
+
+use api\Services\Auth;
+use api\Services\ZermeloAPI;
+use api\Services\ErrorHandler;
+use api\Models\Response;
+use api\Models\ZermeloUser;
+
+try {
+    // Generate a new instance of the Auth and ZermeloAPI class
+    $auth = new Auth();
+
+    // Authenticate via bearer token
+    $auth->authenticate();
+
+    // Check if user type is given
+    if (!isset($_GET['type'])) {
+        // First search for the user in the student list
+        $user = getStudentDetails($userId, $schoolInSchoolYear);
+
+        // If there is no user found in the student list, search for the user in the teacher list
+        if ($user === null) {
+            $user = getTeacherDetails($userId, $schoolInSchoolYear);
+        }
+    } else if ($_GET['type'] === "student") {
+        $user = getStudentDetails($userId, $schoolInSchoolYear);
+    } else if ($_GET['type'] === "teacher") {
+        $user = getTeacherDetails($userId, $schoolInSchoolYear);
+    } else {
+        ErrorHandler::handle("PARAMETER_INVALID","The parameter 'type' is invalid. Please use 'student' or 'teacher'");
+    }
+
+    // Check if the user is found
+    if ($user === null) {
+        ErrorHandler::handle("ZERMELO_USER_NOT_FOUND");
+    }
+
+    // Create response
+    $response = new Response([$user->getUserDetails()]);
+    $response->send();
+} catch (\Exception $e) {
+    ErrorHandler::handle("ERROR", $e->getMessage());
+}
+
+/**
+ * Get the details of a student
+ * @param string|int $userId
+ * @param int $schoolInSchoolYear
+ * @return ZermeloUser|null Returns the user object if the user is found, otherwise null
+ */
+function getStudentDetails($userId, $schoolInSchoolYear): ZermeloUser|null {
+    $zermeloApi = new ZermeloAPI();
+    $studentData = $zermeloApi->getStudentDetails($userId, $schoolInSchoolYear);
+
+    // Check if the Zermelo API returned an error
+    if ($studentData['status'] !== 200) {
+        ErrorHandler::handleZermeloError($studentData);
+    }
+
+    // Check if the Zermelo API returned no data
+    if (count($studentData['data']) === 0) {
+        return null;
+    }
+
+    // Create user object for teacher
+    $user = new ZermeloUser(
+        code: $studentData['data'][0]['student'],
+        firstName: $studentData['data'][0]['firstName'],
+        prefix: $studentData['data'][0]['prefix'] ?? "",
+        lastName: $studentData['data'][0]['lastName'],
+    );
+
+    return $user;
+}
+
+/**
+ * Get the details of a teacher
+ * @param string|int $userId
+ * @param int $schoolInSchoolYear
+ * @return ZermeloUser|null Returns the user object if the user is found, otherwise null
+ */
+function getTeacherDetails($userId, $schoolInSchoolYear): ZermeloUser|null {
+    $zermeloApi = new ZermeloAPI();
+    $teacherData = $zermeloApi->getTeacherDetails($userId, $schoolInSchoolYear);
+
+    // Check if the Zermelo API returned an error
+    if ($teacherData['status'] !== 200) {
+        ErrorHandler::handleZermeloError($teacherData);
+    }
+
+    // Check if the Zermelo API returned no data
+    if (count($teacherData['data']) === 0) {
+        return null;
+    }
+
+    // Create user object for teacher
+    $user = new ZermeloUser(
+        code: $teacherData['data'][0]['employee'],
+        firstName: $teacherData['data'][0]['firstName'],
+        prefix: $teacherData['data'][0]['prefix'] ?? "",
+        lastName: $teacherData['data'][0]['lastName'],
+    );
+
+    return $user;
+}
--- backend/src/routes.php
+++ backend/src/routes.php
@@ -3,23 +3,34 @@
 require_once __DIR__.'/router.php';
 require_once __DIR__.'/Config/config.php';
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+if(DEBUG_MODE) {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+}
 
 // Set headers for all requests
 header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *'); // TODO: CHANGE IN FUTURE TO ONLY ALLOW KNOWN ORIGINS
+header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
 any('/', 'index.php');
 
-get('/v1/school', 'Controllers/SchoolsController.php'); // To be implemented
-get('/v1/school/$schoolInSchoolYear', 'Controllers/SchoolController.php'); // To be implemented
-get('/v1/school/$schoolInSchoolYear/student/$studentId', 'Controllers/StudentController.php');
-get('/v1/school/$schoolInSchoolYear/teacher/$teacherId', 'Controllers/TeacherController.php');
+get('/v1/school/$schoolInSchoolYear/user/$userId', 'Controllers/UserDetailsController.php');
 get('/v1/schedule/$studentId', 'Controllers/ScheduleController.php');
+
+// Future endpoints
+
+//get('/v1/school', 'Controllers/SchoolsController.php'); // To be implemented
+//get('/v1/school/$schoolInSchoolYear', 'Controllers/SchoolController.php'); // To be implemented
+//get('/v1/device', 'Controllers/DeviceController.php'); // To be implemented
+//get('/v1/device/$deviceId', 'Controllers/DeviceController.php'); // To be implemented
+//put('/v1/device/$deviceId', 'Controllers/DeviceController.php'); // To be implemented
+//delete('/v1/device/$deviceId', 'Controllers/DeviceController.php'); // To be implemented
+//post('/v1/device/$deviceId/alive', 'Controllers/DeviceController.php'); // To be implemented
+//post('/v1/device/register', 'Controllers/DeviceController.php'); // To be implemented
+
 
 // Redirect all other request to page
 any('/404','404.php');
