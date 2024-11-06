33e702b1eb4f6101a8ce1f5db189d61f65f9a4f6
David Jongeneel <87093557+DVDdisk7@users.noreply.github.com>
2024-11-06 12:48:36 +0100

    Fix typo

--- backend/docs/swagger-initializer.js
+++ backend/docs/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "http://545959.leerlingsites.nl/pws/api_docs/openapi.json",
+    url: "http://545959.leerlingsites.nl/pws/api-docs/openapi.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
