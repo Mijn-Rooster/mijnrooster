35ab3953a29cafffaf44f233670516afa32a2810
David Jongeneel <david.jongeneel@outlook.com>
2025-02-28 12:41:48 +0100

    Add toggle view functionality to Schedule component and improve tab management

--- frontend/src/renderer/src/components/Schedule/Schedule.svelte
+++ frontend/src/renderer/src/components/Schedule/Schedule.svelte
@@ -6,28 +6,37 @@
   import { core } from "../../stores/core.store";
 
   let weekViewEnabled = $core.weekView;
+  let activeTab = 0;
 
   export let user: UserModel;
+
+  function toggleView() {
+    activeTab = activeTab === 0 ? 1 : 0;
+  }
 </script>
 
 {#if weekViewEnabled}
+  <!-- Hidden button for numpad control -->
+  <button class="hidden" data-numpad="toggle-view" on:click={toggleView}
+    >Toggle View</button
+  >
+
   <Tabs
     tabStyle="underline"
     class="text-center justify-center display:flex p-0 m-0"
-    data-numpad="toggle-view"
     contentClass="pt-4 h-[calc(100dvh-350px)]"
   >
-    <TabItem open divClass="h-full">
-      <div slot="title" class="flex items-center gap-2 text-xl">Dag</div>
-      <ScheduleDayView {user}/>
+    <TabItem divClass="h-full" open={activeTab === 0}>
+      <div slot="title" class="flex items-center gap-2 text-base">Dag</div>
+      <ScheduleDayView {user} />
     </TabItem>
-    <TabItem divClass="h-full">
-      <div slot="title" class="flex items-center gap-2 text-xl">Week</div>
+    <TabItem divClass="h-full" open={activeTab === 1}>
+      <div slot="title" class="flex items-center gap-2 text-base">Week</div>
       <ScheduleWeekView {user} />
     </TabItem>
   </Tabs>
 {:else}
-<div class="h-[calc(100dvh-300px)]">
-  <ScheduleDayView {user} />
-</div>
+  <div class="h-[calc(100dvh-300px)]">
+    <ScheduleDayView {user} />
+  </div>
 {/if}
